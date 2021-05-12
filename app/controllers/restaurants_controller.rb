900a417d59024f53b0db11ce0ae0c6469cd0babb
Chris Boyle <cthboyle6@gmail.com>
2021-05-12 12:19:52 -0400

    created restaurant controller, TODO: controller view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurants.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
