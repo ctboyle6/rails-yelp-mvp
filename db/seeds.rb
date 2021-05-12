78c5aeca52650e1c1f58d97274f552993541c611
Chris Boyle <cthboyle6@gmail.com>
2021-05-12 12:09:41 -0400

    create db seed file, create routes for restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning old data...'
+# delete all restaurants
+Restaurant.delete_all
+# seed 50 new restaurants
+puts 'Adding new restaurants...'
+50.times do
+  restaurant = Restaurant.create(
+    name: Faker::Creature::Bird.plausible_common_name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORY.sample
+  )
+  puts "Restaurant #{restaurant.id} created"
+end
+
+puts 'Done!'
